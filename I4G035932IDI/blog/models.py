1b1f82288ec48072981cfab9f16fb1cf04203661
Chioma Joy <chiomajoy076@gmail.com>
2022-06-24 12:45:08 +0100

    first commit

--- I4G035932IDI/blog/models.py
+++ I4G035932IDI/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+...
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    published_on = models.DateTimeField(auto_now_add = True)
+    
