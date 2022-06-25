7ac885af288b40af601a60499edb4d6d233a6312
Chioma Joy <chiomajoy076@gmail.com>
2022-06-25 19:55:33 +0100

    first commit

--- I4G035932IDI/blog/models.py
+++ I4G035932IDI/blog/models.py
@@ -8,9 +8,11 @@ from django.urls import reverse
 
 ...
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    author = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    published_on = models.DateTimeField(auto_now_add = True)
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey(User, on_delete=models.CASCADE, default='Anonymous')
+    Created_date = models.DateTimeField(auto_now_add=True)
+    Published_date = models.DateTimeField(auto_now_add=True)
+
+        
     
